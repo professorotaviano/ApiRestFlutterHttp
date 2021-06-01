8ae49525659ad76f6a7158a7d00cb9b2f41dc828
FlutLab.io <>
2021-06-01 22:29:03 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/otavianosilverio/appi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.otavianosilverio.appi
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
